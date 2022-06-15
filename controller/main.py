09bdd378eaf07e8e3f10d5c38d5f8d7944e6a327
tlancaster <tlancaster6@gatech.edu>
2022-06-15 15:22:06 -0400

    controller main.py addition

---
+++ controller/main.py
@@ -0,0 +1,4 @@
+import os, sys
+
+if os.path.abspath(os.path.dirname(os.path.dirname(__file__))) not in sys.path:
+    sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
