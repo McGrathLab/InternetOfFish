b8903d29eff40ea8e85b3a8ab1dd5ad8232afcc2
tlancaster <tlancaster6@gatech.edu>
2022-06-15 15:37:00 -0400

    controller main.py addition

--- controller/main.py
+++ controller/main.py
@@ -3,4 +3,5 @@ import os, sys
 if os.path.abspath(os.path.dirname(os.path.dirname(__file__))) not in sys.path:
     sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
-from internet_of_fish.modules import definitions
+from controller.modules.reporter import Reporter
+
