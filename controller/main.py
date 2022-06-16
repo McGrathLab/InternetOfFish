334705bb90eb0db17faea5dfb39b9833136dacd5
chinardankhara <101128521+chinardankhara@users.noreply.github.com>
2022-06-17 01:13:44 +0530

    Update main.py

--- controller/main.py
+++ controller/main.py
@@ -2,10 +2,10 @@ import os, sys
 
 
 
-from modules.reporter2 import Reporter2
+from modules.reporter import Reporter
 
 if os.path.abspath(os.path.dirname(os.path.dirname(__file__))) not in sys.path:
     sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
-server = Reporter2()
+server = Reporter()
 server.accepting_connections()
