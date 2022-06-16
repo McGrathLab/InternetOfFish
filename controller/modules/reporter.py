23616bfc7db3a1ace072a5e5b5eed88de4f4d7da
chinardankhara <101128521+chinardankhara@users.noreply.github.com>
2022-06-17 01:08:32 +0530

    Update reporter.py

--- controller/modules/reporter.py
+++ controller/modules/reporter.py
@@ -1,5 +1,5 @@
 # TODO: write reporter script
-"""this script should take in status information from multiple pi's via socket connections to their watcher processes,
+"""this script takes in status information from multiple pi's via socket connections to their watcher processes,
 parse those status reports, and compile them into a human-readable format/file (possibly a file that is read-only
 to the user, but writeable by this program?).
 
@@ -7,37 +7,48 @@ Should also notice when a particular host suddenly stops pinging, and potentiall
 have the status reports include the email associated with each project, and set up a simple sendgrid notifier.
 
 https://realpython.com/python-sockets/
-
 """
+# import os, sys, inspect
+# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# print(currentdir)
+# parentdir = os.path.dirname(currentdir)
+# grandparentdir = os.path.dirname(parentdir)
 
-from internet_of_fish.modules.utils import gen_utils
+# sys.path.insert(0, parentdir)
+
 import socket
+import json
+import selectors
 
-LOOPBACK_HOST = "127.0.0.1"
-PORT = 13221
+server_host_name = socket.gethostbyname(socket.gethostname())
+port_number = 13221 #Make sure this is the server port number
 
 
-
-class Reporter(metaclass=gen_utils.AutologMetaclass):
-
+class Reporter():
     def __init__(self):
-        self.listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.listening_socket.bind((LOOPBACK_HOST, PORT))
-
-    def main_loop(self):
-        self.listening_socket.listen()
-        conn, addr = self.listening_socket.accept()
+        self.listening_socket = socket.socket(socket.AF_INET,
+                                              socket.SOCK_STREAM)
+        self.listening_socket.bind((server_host_name, port_number))
+        connection_queue_length = 50 #the number of connections that can be held in queue before the system panics
+        self.listening_socket.listen(connection_queue_length)
+        self.listening_socket.setblocking(False)
+        sel = selectors.DefaultSelector()
+        sel.register(self.listening_socket, selectors.EVENT_READ, data=None)
+        print(server_host_name)
 
     def main_func(self):
-        s.listen()
+        """
+        I saw little value in two functions: main_loop and this one because the server listens in a loop
+        by its nature. Thus, everything important is here.
+
+        Returns the dictionary data
+        """
+        serial_data = ''
+        conn, addr = self.listening_socket.accept()
         while True:
-            c, addr = s.accept()
-            print ('Got connection from', addr)
-            c.send('Thank you for connecting'.encode())
-            c.close()
-            break
-
-
-
-
+            print('connected with ', addr)
+            data = conn.recv(4096).decode('utf-8')
+            serial_data += data
+            print(json.loads(serial_data))
+       
 
