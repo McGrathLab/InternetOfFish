470e0960079faffeb899e2319b3926e0dd7d09b4
tlancaster6 <tuckerlancaster@gmail.com>
2022-09-28 09:51:11 -0400

    rolling back config file

--- bin/configure_worker_from_controller.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mdt exec 'git clone http://github.com/McGrathLab/InternetOfFish.git'
-mdt push ~/.config/rclone/rclone.conf /home/mendel/.config/rclone
-mdt exec 'bash ~/InternetOfFish/bin/configure_worker.sh'
